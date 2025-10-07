b565251297f38353e5dd3c936a0177936900b841
adu <515526080@qq.com>
2025-10-07 23:16:46 +0800

    update

--- vless源码/nat64.js
+++ vless源码/nat64.js
@@ -1,12 +1,12 @@
-async function getNat64ProxyIP(domain, nat64Prefix) {
+async function getNat64ProxyIP(remoteAddress, nat64Prefix) {
   let parts
   nat64Prefix = nat64Prefix.slice(1, -1);
-  if (/^\d+\.\d+\.\d+\.\d+$/.test(domain)) {
-    parts = domain.split('.');
-  } else if (domain.includes(':')) {
-    return domain;
+  if (/^\d+\.\d+\.\d+\.\d+$/.test(remoteAddress)) {
+    parts = remoteAddress.split('.');
+  } else if (remoteAddress.includes(':')) {
+    return remoteAddress;
   } else {
-    const dnsQuery = await fetch(`https://1.1.1.1/dns-query?name=${domain}&type=A`, {
+    const dnsQuery = await fetch(`https://1.1.1.1/dns-query?name=${remoteAddress}&type=A`, {
       headers: { 'Accept': 'application/dns-json' }
     });
     const dnsResult = await dnsQuery.json();
@@ -20,5 +20,6 @@ async function getNat64ProxyIP(domain, nat64Prefix) {
   });
   return `[${nat64Prefix}${hex[0]}${hex[1]}:${hex[2]}${hex[3]}]`;
 }
+// Example usage:
 const res = await getNat64ProxyIP('104.16.0.0', '[2602:fc59:b0:64::]');
 console.log(res);
