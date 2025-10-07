e559fb4427854ce5ff916e95fd14a387d49038e9
adu <515526080@qq.com>
2025-10-07 23:06:52 +0800

    Update

--- vless源码/nat64.js
+++ vless源码/nat64.js
@@ -4,19 +4,18 @@ async function getNat64ProxyIP(domain, nat64Prefix) {
   if (/^\d+\.\d+\.\d+\.\d+$/.test(domain)) {
     parts = domain.split('.');
   } else if (domain.includes(':')) {
-    throw new Error('IPv6 no address translation required');
+    return domain;
   } else {
     const dnsQuery = await fetch(`https://1.1.1.1/dns-query?name=${domain}&type=A`, {
       headers: { 'Accept': 'application/dns-json' }
     });
     const dnsResult = await dnsQuery.json();
     const aRecord = dnsResult.Answer.find(record => record.type === 1);
-    if (!aRecord) throw new Error('Invalid IPv4 address segment');
+    if (!aRecord) return;
     parts = aRecord.data.split('.');
   }
   const hex = parts.map(part => {
     const num = parseInt(part, 10);
-    if (num < 0 || num > 255) { throw new Error('Invalid IPv4 address segment'); };
     return num.toString(16).padStart(2, '0');
   });
   return `[${nat64Prefix}${hex[0]}${hex[1]}:${hex[2]}${hex[3]}]`;
